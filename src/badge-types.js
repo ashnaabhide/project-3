278638beb40ece488d380ce5c0ba29ee59cb8f81
ashnaabhide <32545625+ashnaabhide@users.noreply.github.com>
2023-04-30 21:05:45 -0400

    ugh

--- src/badge-types.js
+++ src/badge-types.js
@@ -88,7 +88,7 @@ export class BadgeTypes extends LitElement {
         `;
     }
     updateType() { 
-        const address = new URL('../assets/type.json', import.meta.url).href;
+        const address = new URL('../assets/badgetypes.json', import.meta.url).href;
        fetch (address).then((response) => { 
         if (response.ok){
               return response.json()
